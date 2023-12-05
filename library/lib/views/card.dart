9d8b2b94a164accb21fd09507020fb32c42b2cd8
xmk <289203664@qq.com>
2023-12-05 15:37:19 +0800

    update dependency

--- library/lib/views/card.dart
+++ library/lib/views/card.dart
@@ -1,4 +1,3 @@
-import 'package:figma_squircle/figma_squircle.dart';
 import 'package:flutter/material.dart';
 import 'package:starrail_ui/theme/base.dart';
 import 'package:starrail_ui/theme/colors.dart';
