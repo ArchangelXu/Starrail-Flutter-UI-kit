f0e5601dd21c6dbe04c63a4a2a7175d258457f99
ArchangelXu <289203664@qq.com>
2024-06-05 16:30:46 +0800

    update web

--- example/script/build_web.sh
+++ example/script/build_web.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo 'building web version using canvaskit renderer'
+echo 'building web version'
 # move to dir of this script
 cd "$(dirname "$0")"
 cd .. || exit
-flutter build web --web-renderer canvaskit
+#flutter build web --web-renderer canvaskit
+flutter build web --wasm
+
 open build/web/
+start build\\web
